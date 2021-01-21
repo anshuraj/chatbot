c03024a846c7f23a1ebdb7b3d68add4b8b89f364
Anshu Raj <anshu.raj@razorpay.com>
2021-01-21 21:22:39 +0530

    Web socket intgration. Intermediate changes

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,20 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faPaperPlane,
   faComments,
   faTimes,
 } from '@fortawesome/free-solid-svg-icons';
+import socketIOClient from 'socket.io-client';
 import Message from './Message';
+
+const ENDPOINT = 'http://127.0.0.1:8080/socket.io/';
 
 const Chatbot = () => {
   const [showChatBot, setShowChatBot] = useState(false);
+  const [inputMsg, setInputMsg] = useState('');
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.on('reply', (data) => {
+      setMessages((m) => [
+        ...m,
+        { timestamp: data.timestamp, message: data, sentBy: 'bot' },
+      ]);
+      console.log('✅', data);
+      document
+        .getElementById('scroll-view')
+        .scrollIntoView({ behavior: 'smooth' });
+    });
+
+    return () => socket.disconnect();
+  }, []);
 
   const handleChatBot = () => {
     setShowChatBot(!showChatBot);
   };
 
-  const arr = new Array(20);
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (inputMsg === '') {
+      return;
+    }
+    const socket = socketIOClient(ENDPOINT);
+
+    setMessages((m) => [
+      ...m,
+      { timestamp: Date.now(), message: inputMsg, sentBy: 'user' },
+    ]);
+    setInputMsg('');
+    document
+      .getElementById('scroll-view')
+      .scrollIntoView({ behavior: 'smooth' });
+
+    socket.emit('send', inputMsg);
+  };
+
   return (
     <div className="chatbot-wrapper">
       <div
@@ -39,19 +80,25 @@ const Chatbot = () => {
             <FontAwesomeIcon icon={faTimes} size="lg" />
           </span>
         </header>
-        <div className="chat">
-          {Array.from({ length: 20 }).map((_, idx) => (
-            <Message start={idx % 2 === 0 ? 'left' : 'right'}>
-              This is a message
+        <div className="chat" id="chat">
+          {messages.map((m, idx) => (
+            <Message start={m.sentBy === 'bot' ? 'left' : 'right'}>
+              {m.message}
             </Message>
           ))}
+          <div id="scroll-view"></div>
         </div>
-        <div>
-          <input placeholder="Type your query here" />
-          <button class="send">
+
+        <form onSubmit={sendMessage}>
+          <input
+            placeholder="Type your query here"
+            value={inputMsg}
+            onChange={(e) => setInputMsg(e.target.value)}
+          />
+          <button type="submit" class="send">
             <FontAwesomeIcon icon={faPaperPlane} />
           </button>
-        </div>
+        </form>
         <div className="swag">Powered by Razorbot</div>
       </div>
     </div>
